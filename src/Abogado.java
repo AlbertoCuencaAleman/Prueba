e494fb30cbafccbc32ebad737bc2680636d556f2
lante <alberto.cuenca2@alu.culm.es>
2023-03-09 11:14:24 +0100

    Creacion de mensaje y overrides

--- src/Abogado.java
+++ src/Abogado.java
@@ -1,4 +1,8 @@
 public class Abogado extends Empleado{
+
+    public void mostrarMnesaje(){
+        System.out.println("Estoy en el tribunal");
+    }
     @Override
     public int getVacaciones() {
         return super.getVacaciones();
@@ -6,6 +10,16 @@ public class Abogado extends Empleado{
     @Override
     public double getSalario() {
         return super.getSalario() + 10000;
+    }
+
+    @Override
+    public int getHoras() {
+        return super.getHoras() + 5;
+    }
+
+    @Override
+    public String getMesVacaciones() {
+        return "julio";
     }
 
 }
